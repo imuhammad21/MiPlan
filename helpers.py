e0186210f616353d11b7379880bc24bd7883c8e0
imuhammad21 <imuhammad21@users.noreply.github.com>
2017-12-06 04:02:40 +0000

    20171206T040228Z

--- helpers.py
+++ helpers.py
@@ -4,6 +4,7 @@ import urllib.request
 from flask import redirect, render_template, request, session
 from functools import wraps
 
+
 def login_required(f):
     """
     Decorate routes to require login.
@@ -12,10 +13,8 @@ def login_required(f):
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        # If user is not logged in, redirect to home page
         if session.get("user_id") is None:
             return redirect("/home")
         return f(*args, **kwargs)
     return decorated_function
-
-
-
